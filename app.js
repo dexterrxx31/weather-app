f2ae6b4a3e0f3b5cc3068d5c378c045362c8056e
Riyan Ahmad <riyanahmad99@gmail.com>
2021-07-20 00:28:39 +0530

    Updated Search page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
 });
 
 app.post("/", function (req, res) {
